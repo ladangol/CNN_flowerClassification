c834670d67eba671685d921bf663f28e14e0def6
ladangol <ladangol@buffalo.edu>
2019-09-17 08:36:04 -0400

    Tuning the model

--- CNNFlowerClassification.py
+++ CNNFlowerClassification.py
@@ -57,11 +57,12 @@ def create_CNNmodel(bath_size, epochs, num_classes):
     model.add(Conv2D(64, (5,5)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size = (2,2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
 
     model.add(Flatten())
     model.add(Dense(512))
     model.add(Activation('relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(256))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
